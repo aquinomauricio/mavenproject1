ced82f31ed44b72a86730303e52b9f66afa69565
aquinomauricio <aquinomauricio014@gmail.com>
2023-08-09 10:46:44 -0300

    Initial commit

---
+++ Mavenproject1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author HP
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundillo!!!");
+        System.out.println(" ");
+        System.out.println("lenteja");
+        System.out.println("a ver ahoraaaaas");
+        System.out.println(" ");
+        System.out.println("mas lento q la mierda");
+        System.out.println(" ");
+        System.out.println("a vergqa");
+    }
+}
---
+++ NewClass.java
@@ -0,0 +1,12 @@
+/*
+ *  Kabure Software
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author HP
+ */
+public class NewClass {
+    
+}
